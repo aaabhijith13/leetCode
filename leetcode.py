9ad66228a74c831bb0310362149953af9b92b7ec
root <aavamadev@gmail.com>
2026-01-26 23:01:58 -0500

    leetcode problems

--- leetcode.py
+++ leetcode.py
@@ -44,4 +44,112 @@ class Solution:
                 if grid[row][column] == "1":
                     islands += 1
                     dfc(row, column)
-        return islands
+        return islandsYou are given an m x n matrix board containing letters 'X' and 'O', capture regions that are surrounded:
+
+#Connect: A cell is connected to adjacent cells horizontally or vertically.
+#Region: To form a region connect every 'O' cell.
+#Surround: The region is surrounded with 'X' cells if you can connect the region with 'X' cells and none of the region cells are on the edge of the board.
+#To capture a surrounded region, replace all 'O's with 'X's in-place within the original board. You do not need to return anything.
+
+ 
+
+#Example 1:
+
+#Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+```
+Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+
+Explanation:
+
+
+In the above diagram, the bottom region is not captured because it is on the edge of the board and cannot be surrounded.
+
+Example 2:
+
+Input: board = [["X"]]
+
+Output: [["X"]]
+
+ 
+
+Constraints:
+
+m == board.length
+n == board[i].length
+1 <= m, n <= 200
+board[i][j] is 'X' or 'O'.
+
+
+## Surrounded Regions
+You are given an m x n matrix board containing letters 'X' and 'O', capture regions that are surrounded:
+
+Connect: A cell is connected to adjacent cells horizontally or vertically.
+Region: To form a region connect every 'O' cell.
+Surround: The region is surrounded with 'X' cells if you can connect the region with 'X' cells and none of the region cells are on the edge of the board.
+To capture a surrounded region, replace all 'O's with 'X's in-place within the original board. You do not need to return anything.
+
+ 
+
+Example 1:
+
+Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+
+Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+
+Explanation:
+
+
+In the above diagram, the bottom region is not captured because it is on the edge of the board and cannot be surrounded.
+
+Example 2:
+
+Input: board = [["X"]]
+
+Output: [["X"]]
+
+ 
+
+Constraints:
+
+m == board.length
+n == board[i].length
+1 <= m, n <= 200
+board[i][j] is 'X' or 'O'.
+```
+
+
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        w, l = len(board), len(board[0])
+        visited = [[False]*l for _ in range(w)]
+        print(visited)
+        dire = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        def dfc(row, column):
+            if row < 0 or column < 0 or row >= w or column >= l or board[row][column] != "O" or visited[row][column]:
+                return 
+            visited[row][column] = True
+            for dx, dy in dire:
+                dfc(row + dx, column + dy)
+        
+        for i in range(w):
+            if board[i][0] == "O" and not visited[i][0]:
+                dfc(i, 0)
+            if board[i][l-1] == "O" and not visited[i][l-1]:
+                dfc(i, l-1)
+
+        for j in range(l):
+            if board[0][j] == "O" and not visited[0][j]:
+                dfc(0, j)
+            if board[w-1][j] == "O" and not visited[w-1][j]:
+                dfc(w-1, j)
+
+        for i in range(w):
+            for j in range(l):
+                if board[i][j] != "X" and not visited[i][j]:
+                    board[i][j] = "X"
+
